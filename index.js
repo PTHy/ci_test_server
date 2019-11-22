2a2a38440a16476033fcbcf19428b562dfc4ed32
= <=>
2019-11-22 00:07:02 +0000

    initialize

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 8080;
+const name = process.env.NAME || "World";
+
+app.get('/', (req, res) => {
+	res.json({message: `Hello ${name}`});
+});
+
+app.listen(port, () => {
+	console.log(`Server is listening on ${port}`);
+});
